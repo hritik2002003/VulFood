5177594ff8d38ecf682cadf95192060a7e86ff91
Hritik Raghuvanshi <hritik2002003@gmail.com>
2024-03-18 22:58:41 +0530

    hisudhf

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      Foody, made by{' '}
+      <a
+        href='https://dineshrout.netlify.app'
+        target={'_blank'}
+        className='text-orange-500'
+      >
+        Dinesh Rout
+      </a>
+    </div>
+  );
+};
+export default Footer;
